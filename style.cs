e9962a27e70e730a471fc73a924c29ee84a71048
eliza941 <elizabethkerubo344@gmail.com>
2025-04-10 21:26:31 +0300

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,46 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color:rgba(6, 2, 2, 0.99);
+}
+
+header {
+  background-color: #333;
+  color: BLACK;
+  padding: 1em;
+  text-align: center;
+}
+
+nav a {
+  color: white;
+  margin: 0 10px;
+  text-decoration: none;
+}
+
+.hero {
+  text-align: center;
+  padding: 2em;
+}
+
+.hero img {
+  width: 200px;
+  border-radius: 50%;
+  margin: 1em 0;
+}
+
+.btn {
+  background-color: teal;
+  color: BLACK;
+  padding: 10px 20px;
+  text-decoration: none;
+  border-radius: 5px;
+}
+
+footer {
+  background-color: #333;
+  color: black;
+  padding: 1em;
+  text-align: center;
+  margin-top: 2em;
+}
